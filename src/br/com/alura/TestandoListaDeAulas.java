58acf795290012d03089402cd2d36518e28e0328
Allana Caldas <allanacaldas@gmail.com>
2021-10-07 12:59:04 -0300

    Aula 02 - Lista de objetos: Ordenando com outro critério de ordenação, usando o metodo sort da propria lista, a interface Comparator e as novidades do Java8

--- src/br/com/alura/TestandoListaDeAulas.java
+++ src/br/com/alura/TestandoListaDeAulas.java
@@ -2,6 +2,7 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 //Aula 02 - Lista de Objetos
 public class TestandoListaDeAulas {
@@ -16,26 +17,16 @@ public class TestandoListaDeAulas {
         aulas.add(a1);
         aulas.add(a2);
         aulas.add(a3);
-
         System.out.println(aulas);
 
-        // ->>> Ordenando uma lista de objetos
-        //Collections.sort(aulas);
 
-        /*No exemplo acima,até tentamos ordenar essa mesma lista de objetos usando o método estático sort() da classe
-        Collections, mas esse simplesmente não compila porque ainda falta um critério de comparação: qual dessas duas aulas viria primeiro? qual o critério
-        que pode ordená-las? No exemplo da aula anterior, tratava-se de uma lista de Strings que já tem um critério de comparação entre duas Strings,
-        no qual qualquer pessoa pode comparar dois desses objetos. Isso é feito através do método compareTo.
+        //Ordenando nossos objetos segundo o método sort (que recebe um Comparator) da própria lista (novidade que o Java 8 trouxe)
+        //Duas formas de ordenar seguindo mesmo critério de tempo de Duração das aulas
+        aulas.sort(Comparator.comparing(Aula::getTempo));
+        aulas.sort(Comparator.comparing(aula -> aula.getTempo()));
+        System.out.println(aulas);
 
-        Para resolver o problema precisamos fazer com que nossa classe de Aula também seja um objeto comparável entre si,
-        bastando que ela implemennte a interface Comparable e o seu metodo compareTo
 
-        Depois que a implementamos e sobreescrevemos o metodo compareTo, podemos ordenar nossa lista seguindo critério estabelecido.
-        */
-        Collections.sort(aulas);
-        System.out.println("Lista devidamente ordenada por ordem alfabética do titulo: " + aulas);
-
-        //https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#compareTo-java.lang.String- (Documentação)
 
     }
 }
