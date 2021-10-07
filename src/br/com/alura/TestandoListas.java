7247da34fc6afc1317a008d99bbe7fffd323ed99
Allana Caldas <allanacaldas@gmail.com>
2021-10-07 11:01:45 -0300

    Aula 01 - Trabalhando com ArrayLists: Inserindo comentários e explicações sobre o código

--- src/br/com/alura/TestandoListas.java
+++ src/br/com/alura/TestandoListas.java
@@ -16,23 +16,24 @@ public class TestandoListas {
         aulas.add(aula1);
         aulas.add(aula2);
         aulas.add(aula3);
-        System.out.println(aulas);
+        System.out.println("Aulas: " + aulas);
 
         //Removendo elementos
         aulas.remove(0);
-        System.out.println(aulas);
+        System.out.println("Resultado da lista após a remoção do primeiro item: " + aulas);
 
         //Percorrendo uma lista - Enhanced for (comando foreach)
         for (String aula : aulas) {
-            System.out.println("Aula: " + aula);
+            System.out.println("Aula" + aula);
         }
         //Outra forma de percorrer a lista usando o for
         //Nesse modo, precisamos saber quantos elementos há nessa lista e faremos isso usando o size()
         for (int i = 0; i < aulas.size(); i++) {
-            System.out.println("aula : " + aulas.get(i));
+            System.out.println("Aula " + i + ": " + aulas.get(i));
         }
 
         //Percorrendo elementos da lista com Java 8 (método foreach)
+        //Traduzindo o método abaixo: para cada aula (item da lista), execute as duas linhas de código em seguida (System.out.println)
         aulas.forEach(aula -> {
             System.out.println("Percorrendo:");
             System.out.println("Aula " + aula);
