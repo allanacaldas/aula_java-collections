84711192f6d24f2afceaf65547daed81205ab66a
Allana Caldas <allanacaldas@gmail.com>
2021-10-07 10:43:18 -0300

    Aula 01 - Trabalhando com ArrayLists: Percorrendo a lista usando o metodo forEach do Java 8

--- src/br/com/alura/TestandoListas.java
+++ src/br/com/alura/TestandoListas.java
@@ -22,15 +22,22 @@ public class TestandoListas {
         aulas.remove(0);
         System.out.println(aulas);
 
-        //Percorrendo uma lista
+        //Percorrendo uma lista - Enhanced for (comando foreach)
         for (String aula : aulas) {
             System.out.println("Aula: " + aula);
         }
-        //Outra forma de percorrer a lista
+        //Outra forma de percorrer a lista usando o for
         //Nesse modo, precisamos saber quantos elementos há nessa lista e faremos isso usando o size()
         for (int i = 0; i < aulas.size(); i++) {
             System.out.println("aula : " + aulas.get(i));
         }
+
+        //Percorrendo elementos da lista com Java 8 (método foreach)
+        aulas.forEach(aula -> {
+            System.out.println("Percorrendo:");
+            System.out.println("Aula " + aula);
+        });
+
 
         //Acessando elementos
         String primeiraAula = aulas.get(0);
